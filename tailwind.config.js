0e7c1a3f7757380f2f9cda32a967d1af72aa0669
AlexandreVDW <vandewielealexandre@gmail.com>
2023-09-20 15:46:45 +0200

    premiere save

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        steamBlue: '#2a475e', // Couleur bleue principale de Steam
+        steamDarkBlue: '#1b2838', // Couleur bleue Foncé principale de Steam
+        steamLightBlue:'#66c0f4', // Couleur bleue light principale de Steam
+        steamGreen: '#00adee', // Couleur verte de Steam
+        steamGray: '#3d4b53', // Couleur grise de Steam
+        steamDarkGray: '#171a21', // Couleur grise foncée de Steam
+        steamWhite: '#c7d5e0', // Couleur blanche de Steam
+      },
+    },
+  },
+  plugins: [],
+}
+
