36f859081012b22a7d26b5d10582a94ea90dfabc
AlexandreVDW <vandewielealexandre@gmail.com>
2023-09-21 11:54:12 +0200

    resolution bug merci virginie et pierre

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # consolidation
+
+## Qui a fait le projet : 
+
+Alexandre Vandewiele
+
+## Qu’est-ce que c’est ? 
+
+Un exercice lors de la formation becode keller 6 de charleroi qui est un exercie de js DOM
+
+## Quand as-tu bossé dessus ? 
+
+le 20/09/23 et le 21/09/23
+
+## Où t’étais ? 
+
+au sein des locaux de becode le 20/09/23
+en distanciel de chez moi le 21/09/23
+
+
+## A quoi ça ressemble ? 
+
+A une collection repartir en mobile sur une colonne et sur pc en 3 colonnes
+
+## La progression… C’est fini ?
+
+Le projet initial est completé mais il faudra surement revenu dessus pour l'amélioré au fur et a mesure de la formation
+
+J'aimerai surtout arrivé a ce que les descriptions soit tout de la meme taille qu'importe le contenu mais je galere pas mal en CSS et tailwind m'aide mais pas suffisamment.
+
+## Qu’est-ce que ça contient ? 
+
+du html, l'utilisation du framework tailwindcss et du js pour la création des cards entre autres
+
+## D’où ça vient ?
+
+d’un exercice à faire en classe… #consolidation
+
+## Le lien du projet deployé :
+
+https://alexandrevdw.github.io/consolidation/
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -23,48 +23,62 @@ function sortItems(order) {
   // Réaffichez les objets triés
   objets.forEach((objet) => {
     const card = document.createElement("div");
-      card.classList.add("flex","flex-col","bg-steamBlue","rounded-lg","shadow-md","mb-4","p-4");
+    card.classList.add("flex", "flex-col","justify-between", "bg-steamBlue", "rounded-lg", "shadow-md", "mb-4", "p-4");
+
+    // Créer une div pour le contenu principal
+    const contentDiv = document.createElement("div");
+    contentDiv.classList.add("mb-4"); // Ajoutez des classes de marge au besoin
 
     const nom = document.createElement("h2");
-      nom.textContent = objet.nom;
-      nom.classList.add("text-xl", "font-semibold", "text-center", "mb-2","text-steamWhite");
-      card.appendChild(nom);
+    nom.textContent = objet.nom;
+    nom.classList.add("text-xl", "font-semibold", "text-center", "mb-2", "text-steamWhite");
+    contentDiv.appendChild(nom);
 
     const image = document.createElement("img");
-      image.src = objet.image;
-      image.classList.add("w-full", "h-auto");
-      card.appendChild(image);
+    image.src = objet.image;
+    image.classList.add("w-full", "h-auto");
+    contentDiv.appendChild(image);
 
     const parution = document.createElement("p");
-      parution.textContent = `Parution: ${objet.parution}`;
-      parution.classList.add("text-steamWhite", "py-2");
-      card.appendChild(parution);
+    parution.textContent = `Parution: ${objet.parution}`;
+    parution.classList.add("text-steamWhite", "py-2");
+    contentDiv.appendChild(parution);
 
     const description = document.createElement("p");
-      description.textContent = objet.description;
-      description.classList.add("py-2", "text-steamWhite");
-      card.appendChild(description);
+    description.textContent = objet.description;
+    description.classList.add("py-2", "text-steamWhite");
+    contentDiv.appendChild(description);
+
+    // Ajoutez la div de contenu principal à la carte
+    card.appendChild(contentDiv);
+
+    // Créez une div pour les informations d'édition et de développement
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("py-2","flex", "flex-col"); // Ajoutez des classes de marge au besoin
 
     const edition = document.createElement("p");
-      edition.textContent = `Édition: ${objet.edition}`;
-      edition.classList.add("py-2", "text-steamLightBlue");
-      card.appendChild(edition);
+    edition.textContent = `Édition: ${objet.edition}`;
+    edition.classList.add("text-steamLightBlue");
+    infoDiv.appendChild(edition);
 
     const developpement = document.createElement("p");
-      developpement.textContent = `Développement: ${objet.developpement}`;
-      developpement.classList.add("py-2", "text-steamLightBlue");
-      card.appendChild(developpement);
+    developpement.textContent = `Développement: ${objet.developpement}`;
+    developpement.classList.add("py-2","text-steamLightBlue");
+    infoDiv.appendChild(developpement);
 
     const more = document.createElement("a");
-      more.textContent = objet.more;
-      more.href = objet.lien;
-      more.target = "_blank";
-      more.classList.add("py-2","px-1", "text-steamGreen", "bg-steamDarkBlue","rounded-lg","text-center", "hover:border", "hover:border-steamWhite", "hover:p-2", "hover:transition");
-      card.appendChild(more);
-    
+    more.textContent = objet.more;
+    more.href = objet.lien;
+    more.target = "_blank";
+    more.classList.add("py-2", "px-1", "text-steamGreen", "bg-steamDarkBlue", "rounded-lg", "text-center", "hover:border", "hover:border-steamWhite", "hover:p-2", "hover:transition");
+    infoDiv.appendChild(more);
+
+    // Ajoutez la div d'informations à la carte
+    card.appendChild(infoDiv);
+
     collection.appendChild(card);
-      
-  });
+});
+
 }
 
 // Initialisez le tri avec l'ordre alphabétique croissant au chargement de la page
